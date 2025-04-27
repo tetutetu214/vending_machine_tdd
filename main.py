28ce190099fcaa3b037ce9e5e4693a2e17ecc09d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2025-04-27 13:15:12 +0000

    new main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# VendingMachineのクラス
+class VendingMachine:
+    # 初期化
+　　def __init__(self):
+        self.balance = 0
+
+    # 初期残高は0円を返す
+    def get_balance(self):
+        return self.balance
+
+    # コインを投入すると残高が増える
+    def insert_coin(self, amount):
+        self.balance += amount
