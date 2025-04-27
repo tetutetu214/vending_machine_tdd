3c3941dc141ac6aefe6d99a27bfc852cc2247290
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2025-04-27 13:54:54 +0000

    add main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ class VendingMachine:
     # 初期化
     def __init__(self):
         self.balance = 0
+        self.products = [
+            {"id": 1, "name": "コーラ", "price": 150},
+            {"id": 2, "name": "お茶", "price": 130},
+            {"id": 3, "name": "水", "price": 100}
+        ]
 
     # 初期残高は0円を返す
     def get_balance(self):
@@ -11,3 +16,34 @@ class VendingMachine:
     # コインを投入すると残高が増える
     def insert_coin(self, amount):
         self.balance += amount
+
+    # 商品を返す
+    def get_products(self):
+        return self.products
+
+    # 商品を探す
+    def purchase(self, product_id):
+        product = None
+        for p in self.products:
+            if p["id"] == product_id:
+                product
+
+        if product is None:
+            return {
+                "success": False,
+                "message": "商品が見つかりません"
+            }
+
+        # 残高チェック
+        if self.balance < product["price"]:
+            return {
+                "success": False,
+                "message": "残高不足です"
+            }
+
+        # 購入処理
+        self.balance -= product["price"]
+        return {
+            "success": True,
+            "product": product
+        }
